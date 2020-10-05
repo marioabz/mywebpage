b208edca664dc8d1affa2018ac64e7c26676331e
Mario Briseño <mario.briseno7@gmail.com>
2020-10-04 23:55:17 -0500

    adding text to page

---
+++ src/components/languages.js
@@ -0,0 +1,17 @@
+export const lang_en = {
+    career: "Software Engineer",
+    info: "As a self-taught software engineer Mario has over 3 years of experience working with backend technologies. Mario has been working with Blockchain technologies the last 2 years at a cryptocurrencies exchange startup. Currently he's improving his data structures and algorithms skills as well as his systems design knowledge. He "
+}
+export const lang_esp = ""
+export const lang_chin = ""
+
+export const tools = [
+    "Python 3",
+    "NodeJS",
+    "Redis",
+    "Bitcoin Blockchain",
+    "Ethereum Blockchain",
+    "Django Rest Framework",
+    "MySQL, PosgreSQL, DynamoDB",
+    "AWS tools (EC2, EBS, RDS, S3, Lambdas, API Gateway, VPC)"
+]
