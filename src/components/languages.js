80ffca26ed0a4e362f1f1cb783740e0d6977fa05
Mario Briseño <mario.briseno7@gmail.com>
2020-10-05 23:52:53 -0500

    Completing my personal info

--- src/components/languages.js
+++ src/components/languages.js
@@ -1,17 +1,17 @@
 export const lang_en = {
     career: "Software Engineer",
-    info: "As a self-taught software engineer Mario has over 3 years of experience working with backend technologies. Mario has been working with Blockchain technologies the last 2 years at a cryptocurrencies exchange startup. Currently he's improving his data structures and algorithms skills as well as his systems design knowledge. He "
+    info: "As a self-taught software engineer Mario has over 3 years of experience working with backend technologies. Mario has been working with Blockchain technologies the last 2 years at a cryptocurrencies exchange startup. Currently he is improving his data structures and algorithms skills as well as his systems design knowledge. He enjoys doing weight lifting, reading books, occasional beers and staying up to date with techonology trends. He does not like learning by watching videos."
 }
 export const lang_esp = ""
 export const lang_chin = ""
 
 export const tools = [
-    "Python 3",
-    "NodeJS",
     "Redis",
+    "Python3 with Django and DRF",
+    "NodeJS with ExpressJS",
     "Bitcoin Blockchain",
-    "Ethereum Blockchain",
-    "Django Rest Framework",
+    "Ethereum Blockchain with Web3",
+    "Truffle, Ganache and IPFS",
     "MySQL, PosgreSQL, DynamoDB",
     "AWS tools (EC2, EBS, RDS, S3, Lambdas, API Gateway, VPC)"
 ]
